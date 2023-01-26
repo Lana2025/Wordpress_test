13ec1c95139530649174b6b36ffefa1e02442628
Lana2025 <havryluiklana@gmail.com>
2023-01-26 14:02:12 +0000

    layout on bootstrap

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/*  ---------------------------------------------------
+Template Name: Lana
+
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+   
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".offcanvas-menu-overlay, .offcanvas__close").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".header__menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Accordin Active
+    --------------------*/
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().removeClass('active');
+    });
+
+   
+
+})(jQuery);
